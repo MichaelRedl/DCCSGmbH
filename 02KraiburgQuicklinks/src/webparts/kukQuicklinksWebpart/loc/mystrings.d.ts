8ef6438f0cb9f0915ea63a6a79dfbb879f2926f5
Michael  Redl <m.redl@michael-redl.com>
2023-12-05 09:20:00 +0100

    commit

---
+++ 02KraiburgQuicklinks/src/webparts/kukQuicklinksWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IKukQuicklinksWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'KukQuicklinksWebpartWebPartStrings' {
+  const strings: IKukQuicklinksWebpartWebPartStrings;
+  export = strings;
+}
