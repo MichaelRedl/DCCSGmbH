35dbcc0290f76f29278f56f71b5d8b86b6ed6b71
Michael  Redl <m.redl@michael-redl.com>
2024-02-01 16:22:34 +0100

    added multilevel navigation

---
+++ 07KUKMultiLevelNavigation/src/extensions/multilevelNavigation/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IMultilevelNavigationApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'MultilevelNavigationApplicationCustomizerStrings' {
+  const strings: IMultilevelNavigationApplicationCustomizerStrings;
+  export = strings;
+}
